e1fe6edf5ebd66ac6cb30b86ecf7dd792976ce18
unknown <ericaamanatad@gmail.com>
2026-01-29 17:27:25 +0800

    sample update - change name in Hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
   return (
     <div id='home' className='hero d-flex flex-column align-items-center gap-4'>
         <img src={profile_img} alt="profile_img" width={250} height={250} />
-        <h1><span>I'm Erica Manatad,</span> a Front-End Developer</h1>
+        <h1><span>I'm Erica,</span> a Front-End Developer</h1>
         <p>I'm a passionate Front-End Developer who loves to create beautiful and functional websites.</p>
         <div className="hero-action d-flex align-items-center justify-content-center">
             <div className="hero-connect "><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect With Me</AnchorLink></div>
